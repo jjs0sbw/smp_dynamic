c89424aa0895c19aa3709e30b726980861cf58ae
Joseph Simpson <jjs0sbw@gmail.com>
2020-02-01 05:33:22 -0800

    Review class code and cluster code.

--- artclass.py
+++ artclass.py
@@ -1,4 +1,4 @@
-# Copyright 2008 - 2012 Joseph J. Simpson GPL-3.0-or-later
+# Copyright 2008 - 2020 Joseph J. Simpson GPL-3.0-or-later
 #   This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
 #    the Free Software Foundation, either version 3 of the License, or
