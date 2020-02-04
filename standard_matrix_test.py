ae11f51dba92627a776e4a3449c8209423efd950
Joseph Simpson <jjs0sbw@gmail.com>
2020-02-04 10:48:24 -0800

    Clean up the commnets

--- standard_matrix_test.py
+++ standard_matrix_test.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Copyright 2008 - 2020 Joseph J. Simpson GPL-3.0-or-later
+# Copyright 2020 Joseph J. Simpson GPL-3.0-or-later
 #   This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
 #    the Free Software Foundation, either version 3 of the License, or
